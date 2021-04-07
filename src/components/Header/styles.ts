043b95fa6905859bd65bf33f906e55ccfc93657a
ArieleM <ariele_apm@outlook.com>
2021-04-07 18:50:04 -0300

    Adiciona e estiliza o NewTransactionModal

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
   background: var(--blue);
@@ -7,7 +7,7 @@ export const Container = styled.div`
 export const Content = styled.div`
   max-width: 1120px;
   margin: 0 auto;
-  
+
   padding: 2rem 1rem 12rem;
   display: flex;
   align-items: center;
@@ -17,14 +17,14 @@ export const Content = styled.div`
     font-size: 1rem;
     color: #fff;
     background-color: var(--blue-light);
-    border:0;
+    border: 0;
     padding: 0 2rem;
     border-radius: 0.25rem;
     height: 3rem;
-    
+
     transition: filter 0.2s;
 
-    &:hover{
+    &:hover {
       filter: brightness(0.9);
     }
   }
